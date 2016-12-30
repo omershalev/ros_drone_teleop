5a3391c0e18ebe59519f4d61bb0606d7c02785d2
Omer Shalev <omer.shalev@campus.technion.ac.il>
2016-12-30 21:02:05 +0200

    Added capabilities to Bebop: return to home + change camera orientation

--- ros_drone_teleop/scripts/drone_keyboard_teleop/bebop.py
+++ ros_drone_teleop/scripts/drone_keyboard_teleop/bebop.py
@@ -4,7 +4,8 @@ import rospy
 import os
 from drone_keyboard_teleop.keyboard_teleop_base import KeyboardTeleop
 from std_msgs.msg import Empty as EmptyMsg
-
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Twist
 
 class BebopKeyboardTeleop(KeyboardTeleop):
     def __init__(self, namespace):
@@ -13,7 +14,19 @@ class BebopKeyboardTeleop(KeyboardTeleop):
         self.takeoff_publisher = rospy.Publisher(self.namespace + 'takeoff', EmptyMsg, queue_size=10)
         self.land_publisher = rospy.Publisher(self.namespace + 'land', EmptyMsg, queue_size=10)
         self.reset_publisher = rospy.Publisher(self.namespace + 'reset', EmptyMsg, queue_size=10)
+        self.navigate_home_publisher = rospy.Publisher(self.namespace + 'autoflight/navigate_home', Bool, queue_size=10)
         self.flat_trim_publisher = rospy.Publisher(self.namespace + 'flattrim', EmptyMsg, queue_size=10)
+        self.camera_control_publisher = rospy.Publisher(self.namespace + 'camera_control', Twist, queue_size=10)
+
+    def print_model_specific_operations(self):
+        print '================ Bebop  Operations ================='
+        print '\'f\' - flat trim'
+        print '\'t\' - takeoff'
+        print '\'d\' - land'
+        print '\'e\' - emergency shutdown'
+        print '\'h\' - return to home'
+        print '\'c\' - change camera orientation'
+
 
     def check_model_specific_operations(self, ch):
         if ch == 't':
@@ -38,7 +51,24 @@ class BebopKeyboardTeleop(KeyboardTeleop):
                 msg = EmptyMsg()
                 rospy.sleep(1)
                 self.reset_publisher.publish(msg)
+        elif ch == 'h':
+            twist = self.create_twist_message(0,0,0,0,0,0)
+            self.send_twist(twist)
+            msg = Bool(True)
+            rospy.sleep(1)
+            self.navigate_home_publisher.publish(msg)
         elif ch == 'f':
             msg = EmptyMsg()
             rospy.sleep(1)
-            self.flat_trim_publisher.pub(msg)
+            self.flat_trim_publisher.publish(msg)
+        elif ch == 'c':
+            os.system('clear')
+            print 'Enter the desired tilt in degrees'
+            tilt = input()
+            print 'Enter the desired pan in degrees'
+            pan = input()
+            if not isinstance(tilt, int) or not isinstance(pan, int):
+                print 'Invalid input'
+                return
+            twist = self.create_twist_message(0,0,0,0,tilt,pan)
+            self.send_twist(twist)
